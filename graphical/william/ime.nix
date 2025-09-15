89b51a29ebaaac4c1176b11f5287c9553aa576cd
William Boulanger <willbou2@gmail.com>
2025-09-14 21:13:29 -0400

    Declarative fcitx config using new declarative module

--- graphical/william/ime.nix
+++ graphical/william/ime.nix
@@ -5,11 +5,42 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      fcitx5-hangul
-      fcitx5-chinese-addons
-    ];
+    fcitx5 = {
+      settings = {
+        globalOptions = {
+          "Hotkey/TriggerKeys" = {
+            "0" = "Super+space";
+          };
+        };
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            "DefaultIM" = "hangul";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-us";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "hangul";
+            Layout = "";
+          };
+          "Groups/0/Items/2" = {
+            Name = "mozc";
+            Layout = "";
+          };
+          GroupOrder = {
+            "0" = "Default";
+          };
+        };
+       };
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-hangul
+        fcitx5-chinese-addons
+      ];
+    };
   };
 }
